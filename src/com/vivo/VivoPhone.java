706f6f38ae0552496e6e1716410f7f12dbd0bb7d
sivasankarr1 <sivasankarr1@outlook.com>
2023-12-27 13:48:08 +0530

    Tamil Push

--- src/com/vivo/VivoPhone.java
+++ src/com/vivo/VivoPhone.java
@@ -5,7 +5,7 @@ public class VivoPhone {
 	public static void main(String[] args) {
 		String s="Vivo y56";
 		System.out.println(s.contains("o"));
-
+		System.out.println("Tamil using Vivo phone");
 	}
 
 }
